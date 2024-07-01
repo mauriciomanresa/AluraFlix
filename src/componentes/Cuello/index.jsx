107de1d7e3ecc8f52236eb0cd397d29c93629710
CMM_Design <mauriciomanresa@hotmail.com>
2024-07-01 11:26:53 -0300

    Solución de errores del deploy

--- src/componentes/Cuello/index.jsx
+++ src/componentes/Cuello/index.jsx
@@ -2,10 +2,11 @@ import styles from "./Cuello.module.css";
 
 //Función que define la imagen entre el nav y el body.
 function Cuello({ img, color }) {
+  const nombreProyecto = "AluraFlix"
   return (
     <div
       className={styles.imagenSuperior}
-      style={{ backgroundImage: `url('/img/imagen-superior.jpg')` }}
+      style={{ backgroundImage: `url('/${nombreProyecto}/img/imagen-superior.jpg')` }}
     ></div>
   );
 }
